03330602774deee6faabd5576b4b37a013f03ed1
arun-kumar-js <srivar63@outlook.com>
2025-01-09 14:02:35 +0530

    Add recipeRoutes module

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
-const recipeRoutes = require('recipeRoutes');
+const recipeRoutes = require('./routes/recipeRoutes');
 
 require('dotenv').config();
 
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 
 app.use('/api/recipes', recipeRoutes);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
