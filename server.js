a1edfd5ba9b1f0191e667c12e2a39992489ff4c3
arun-kumar-js <srivar63@outlook.com>
2025-01-09 11:24:06 +0530

    again commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 
 app.use('/api/recipes', recipeRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
